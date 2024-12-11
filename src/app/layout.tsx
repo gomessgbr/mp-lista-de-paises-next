18f6bcc9576d57809a423a4a9c25844e39449db9
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-12-11 11:55:13 -0300

    refactor: home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Nunito_Sans } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
+import worldIcon from "./_assets/world.svg";
 import "./globals.css";
+import Image from "next/image";
 
 const nunitoSans = Nunito_Sans({
   variable: "--font-nunito-sans",
@@ -19,7 +21,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${nunitoSans.variable}  antialiased`}>{children}</body>
+      <body className={`${nunitoSans.variable}  antialiased`}>
+        <main className="bg-gray-100 min-h-screen flex flex-col items-center">
+          <nav className="w-full bg-white h-16 flex items-center justify-center">
+            <section className="container flex items-center gap-3">
+              <Image
+                width={48}
+                height={48}
+                src={worldIcon}
+                alt="Logo da aplicação - emoji de globo"
+              />
+              <h1 className="font-bold text-2xl">Países do mundo</h1>
+            </section>
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,25 @@
 "use client";
 
 import { useGetAllContries } from "@/hooks/useGetAllCountries";
-import Image from "next/image";
-import worldIcon from "./_assets/world.svg";
+
 import CountriesCard from "./_components/CoutriesCard";
 
 export default function Home() {
   const { countries } = useGetAllContries();
 
   return (
-    <main className="bg-gray-100 min-h-screen flex flex-col items-center">
-      <nav className="w-full h-16 flex place-content-center bg-white py-10">
-        <section className="container flex items-center gap-3">
-          <Image src={worldIcon} alt="mundo" width={48} height={48} />
-          <h1 className="text-2xl font-bold">Países do mundo</h1>
-        </section>
-      </nav>
-
-      <section className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2 mt-12">
-        {countries.map((countrie) => {
-          return (
-            <CountriesCard
-              key={countrie.name.common}
-              name={countrie.name.common}
-              ptName={countrie.translations.por.official}
-              flag={countrie.flags.svg}
-              flagAlt={countrie.flags.alt || countrie.altSpellings[0]}
-            />
-          );
-        })}
-      </section>
-    </main>
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2 mt-16">
+      {countries.map((countrie) => {
+        return (
+          <CountriesCard
+            key={countrie.name.common}
+            name={countrie.name.common}
+            ptName={countrie.translations.por.official}
+            flag={countrie.flags.svg}
+            flagAlt={countrie.flags.alt || countrie.altSpellings[0]}
+          />
+        );
+      })}
+    </section>
   );
 }
